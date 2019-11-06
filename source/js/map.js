74b63cb9fc7ed3c751cb2418795091f5a0980518
garold664 <garold664@gmail.com>
2019-11-06 15:19:19 +0300

    inserts google map through js api

---
+++ source/js/map.js
@@ -0,0 +1,18 @@
+// Google Maps
+var map;
+function initMap() {
+  var myLatLng = {lat: 59.938872, lng: 30.323047};
+  var image = 'img/map-pin.png';
+
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: myLatLng,
+    zoom: 17
+  });
+
+  var marker = new google.maps.Marker({
+    position: myLatLng,
+    map: map,
+    icon: image,
+    title: 'Cat Energy'
+});
+}
